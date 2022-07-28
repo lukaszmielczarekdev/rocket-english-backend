bdc786ffc0ba041dcb23aef43b640ab64f52605f
lukaszmielczarekdev <lukaszmielczarekdev@gmail.com>
2022-07-28 23:47:01 +0200

    reset progress feature

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ import {
   signup,
   deleteUser,
   updateProgress,
+  resetProgress,
   saveGame,
 } from "../controllers/users.js";
 import auth from "../middleware/auth.js";
@@ -16,6 +17,7 @@ router.post("/signup", signup);
 router.post("/externalsignin", externalSignin);
 router.delete("/:id", auth, deleteUser);
 router.patch("/updateprogress", auth, updateProgress);
+router.patch("/resetprogress", auth, resetProgress);
 router.patch("/savegame", auth, saveGame);
 
 export default router;
